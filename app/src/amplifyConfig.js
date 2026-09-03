e53a6fc664301f247ea7c8731dfd8114bde21be6
Jhanvi Mittal <jhanvi.mittal.05@gmail.com>
2026-09-03 15:39:19 -0400

    amplify auth

---
+++ app/src/amplifyConfig.js
@@ -0,0 +1,9 @@
+export const amplifyConfig = {
+  Auth: {
+    Cognito: {
+      userPoolId: 'us-east-2_orSsOuT3T',
+      userPoolClientId: '43n2r59trrr9r1kbdc5d241j0',
+      region: 'us-east-2',
+    },
+  },
+}
